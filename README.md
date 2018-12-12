b0414f47753e5569d38d9fd30b2562c413a12537
Glitch (5-fcc-file-metadata-microservice) <none>
2018-12-12 08:24:29 +0000

    🗻🎡 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# FCC-File-Metadata-Microservice
-Copy of a Glitch project developed for the FCC Challenge: Apis and Microservices Projects - File Metadata Microservice
+
+# API Project: File Metadata Microservice for freeCodeCamp
+
+###    User stories:
+1. Puedo cargar un objeto "form" que incluye un archivo.
+2. El input field del archivo está denominado "upfile" en el HTML (mantener para testing).
+3. Cuando cargo el archivo, se devuelve 'nombre', 'tipo' y 'tamaño(bytes)' en la respuesta JSON.
+
+### Uso :
+* Ve a [APP](https://5-fcc-file-metadata-microservice.glitch.me) y sube un archivo usando el formulario
+
+### Hint:
+* Par manejar la subida de archivos deberás utilizar el paquete nmp [multer](https://www.npmjs.com/package/multer)
---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+//Uso Multer, una librería para gestión de archivos a través de HTML5 forms
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+
+//<form action="/profile" method="post" enctype="multipart/form-data">
+//  <input type="file" name="avatar" />
+//</form>
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `upfile` file
+  res.json({name:req.file.originalname, type:req.file.mimetype,size:req.file.size});
+  // req.body will hold the text fields, if there were any
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
